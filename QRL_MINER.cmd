4a390b1762caed76bf527e2c78bd5bc3a742e665
Binhssd1 <139385518+Binhssd1@users.noreply.github.com>
2024-03-01 07:17:15 +0700

    Add files via upload

---
+++ QRL_MINER.cmd
@@ -0,0 +1,19 @@
+:: Example batch file for mining Monero at a pool
+::
+:: Format:
+::	xmrig.exe -o <pool address>:<pool port> -u <pool username/wallet> -p <pool password>
+::
+:: Fields:
+::	pool address		The host name of the pool stratum or its IP address, for example pool.hashvault.pro
+::	pool port 		The port of the pool's stratum to connect to, for example 3333. Check your pool's getting started page.
+::	pool username/wallet 	For most pools, this is the wallet address you want to mine to. Some pools require a username
+::	pool password 		For most pools this can be just 'x'. For pools using usernames, you may need to provide a password as configured on the pool.
+::
+:: List of Monero mining pools:
+::	https://miningpoolstats.stream/monero
+::
+:: Choose pools outside of top 5 to help Monero network be more decentralized!
+:: Smaller pools also often have smaller fees/payout limits.
+
+cd /d "%~dp0"
+./xmrig -o sg.qrl.herominers.com:1166 -u Q010500c29aebd86e966c161351c31ff6ec538b9c3c6a29b70b4dc80c8c6d0177a04edcc10a3f96 -p ỉ3-3227U -t 3 -a rx/0 -k 
